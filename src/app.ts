755455a4a89b4cb2cf5fb699fd3dad900fb2abf0
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2022-03-18 12:37:50 +0530

    Implement not found route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import compression from "compression";
 import routes from "./routes";
 import dotenv from "dotenv";
+import notFound from "./errors/notFound";
 dotenv.config({ path: ".env.dev" });
 
 const port = process.env.PORT || 3000;
@@ -12,5 +13,6 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(routes);
+app.use(notFound);
 
 export default app;
