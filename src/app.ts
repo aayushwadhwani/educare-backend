742786c577c91c363b7bfb895eb0158972c397db
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2022-03-19 13:34:47 +0530

    Start using errorHandler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import compression from "compression";
 import routes from "./routes";
 import dotenv from "dotenv";
 import notFound from "./errors/notFound";
+import errorHandlerMiddleware from "./middlewares/errorHandler";
 dotenv.config({ path: ".env.dev" });
 
 const port = process.env.PORT || 3000;
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(routes);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 export default app;
--- src/errors/notFound.ts
+++ src/errors/notFound.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import createError from "../response/fail";
 
-const notFound = (req: Request, res: Response) => {
-    res.status(404).json({ message: "Not Found" });
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+    return next(createError(`Cannot find ${req.method} ${req.originalUrl} on this server`));
 };
 
 export default notFound;
