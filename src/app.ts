bb580a387e76f69c45af864bbe26ddae30215d37
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2022-04-26 23:21:45 +0530

    Add cors settings

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import routes from "./routes";
 import dotenv from "dotenv";
 import notFound from "./errors/notFound";
 import errorHandlerMiddleware from "./middlewares/errorHandler";
+import cors from "cors";
 
 dotenv.config({ path: ".env.dev" });
+const corsOptions: cors.CorsOptions = {
+    origin: "*",
+    methods: "GET ,HEAD, POST ,DELETE ,PATCH ,PUT",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
 
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -35,6 +42,10 @@ app.set("port", port);
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use(cors(corsOptions));
+app.options("*", cors);
+
 app.use(routes);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
